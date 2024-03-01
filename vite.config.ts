06e15bffe8eff8e87cb0bd96355a9aae707a5050
hfxz <xuan__zai@outlook.com>
2024-03-01 16:27:21 +0800

    引入 unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,11 @@ export default defineConfig({
     vueJsx(),
     AutoImport({
       imports: ['vue', 'vue-router', 'pinia', 'vitest'],
+      dts: true
+    }),
+    Components({
+      dirs: ['src/components'],
+      resolvers: [],
       dts: true
     })
   ],
